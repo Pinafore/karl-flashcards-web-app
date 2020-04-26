4a33bfcade59f7da19699a483979f6db900c4e11
Nonameentered <matthewshu88@gmail.com>
2020-04-26 15:07:42 -0400

    fix user tests, authentication allows username

--- backend/app/app/crud/crud_fact.py
+++ backend/app/app/crud/crud_fact.py
@@ -4,14 +4,13 @@ from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
-from app.models.fact import Fact
-from app.schemas.fact import FactCreate, FactUpdate
+from app import crud, models, schemas
 
 
-class CRUDFact(CRUDBase[Fact, FactCreate, FactUpdate]):
+class CRUDFact(CRUDBase[models.Fact, schemas.FactCreate, schemas.FactUpdate]):
     def create_with_owner(
-        self, db: Session, *, obj_in: FactCreate, owner_id: int
-    ) -> Fact:
+        self, db: Session, *, obj_in: schemas.FactCreate, owner_id: int
+    ) -> models.Fact:
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data, owner_id=owner_id)
         db.add(db_obj)
@@ -20,15 +19,27 @@ class CRUDFact(CRUDBase[Fact, FactCreate, FactUpdate]):
         return db_obj
 
     def get_multi_by_owner(
-        self, db: Session, *, owner_id: int, skip: int = 0, limit: int = 100
-    ) -> List[Fact]:
+        self, db: Session, *, user: models.User, skip: int = None, limit: int = None
+    ) -> List[models.Fact]:
         return (
             db.query(self.model)
-            .filter(Fact.owner_id == owner_id)
-            .offset(skip)
-            .limit(limit)
-            .all()
+                .filter(models.Fact.user_id == user.id)
+                .offset(skip)
+                .limit(limit)
+                .all()
         )
 
+    # def remove(self, db: Session, *, fact_id: int):
+    #     new_suspend_create = SuspendedCreate(
+    #         date_suspended=datetime.now(timezone('UTC')),
+    #         fact_id=fact_id,
+    #         delete=True,
+    #     )
+    #     new_suspend = Suspended(**new_suspend_create.dict())
+    #     db_session.add(new_suspend)
+    #     db_session.commit()
+    #
+    #     return db_session.query(models.Fact).filter(models.Fact.id == fact_id).first()
 
-fact = CRUDFact(Fact)
+
+fact = CRUDFact(models.Fact)
--- backend/app/app/tests/crud/test_user.py
+++ backend/app/app/tests/crud/test_user.py
@@ -61,7 +61,7 @@ def test_check_if_user_is_superuser(db: Session) -> None:
     password = random_lower_string()
     username = random_lower_string()
     user_in = SuperUserCreate(email=email, username=username, password=password, is_superuser=True)
-    user = crud.user.create(db, obj_in=user_in)
+    user = crud.user.super_user_create(db, obj_in=user_in)
     is_superuser = crud.user.is_superuser(user)
     assert is_superuser is True
 
@@ -71,7 +71,7 @@ def test_check_if_user_is_superuser_normal_user(db: Session) -> None:
     password = random_lower_string()
     username = random_lower_string()
     user_in = UserCreate(email=email, username=username, password=password)
-    user = crud.user.super_user_create(db, obj_in=user_in)
+    user = crud.user.create(db, obj_in=user_in)
     is_superuser = crud.user.is_superuser(user)
     assert is_superuser is False
 
