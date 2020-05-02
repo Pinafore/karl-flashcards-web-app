ae177d64a91e6edae195e8a927fc7b729a130d94
Nonameentered <matthewshu88@gmail.com>
2020-05-02 17:17:51 -0400

    Fix frontend jest tests

--- frontend/tests/unit/upload-button.spec.ts
+++ frontend/tests/unit/upload-button.spec.ts
@@ -1,6 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
 import UploadButton from "@/components/UploadButton.vue";
 import "@/plugins/vuetify";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 describe("UploadButton.vue", () => {
   it("renders props.title when passed", () => {
