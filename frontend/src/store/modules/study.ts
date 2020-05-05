ab9df5e9fb59d3d68d7dfb6c31cf150c247142d6
Nonameentered <matthewshu88@gmail.com>
2020-05-05 02:54:58 -0400

    Partial rewrite of study functionality

--- frontend/src/store/modules/study.ts
+++ frontend/src/store/modules/study.ts
@@ -1,26 +1,227 @@
 import { api } from "@/api";
-import { VuexModule, Module, Mutation, Action } from "vuex-module-decorators";
-import { IComponents } from "@/interfaces";
+import { Action, Module, Mutation, VuexModule } from "vuex-module-decorators";
+import { IComponents, IStudyShow } from "@/interfaces";
 import { mainStore } from "@/utils/store-accessor";
 
 @Module({ name: "study" })
 export default class StudyModule extends VuexModule {
   facts: IComponents["Fact"][] = [];
+  decks: IComponents["Deck"][] = [];
+  schedule: IComponents["Schedule"][] = [];
+  recommendation = false;
+  show: IStudyShow = { text: "loading", enable_report: false, enable_actions: false };
+  frontTime = 0;
+  time = 0;
+  timer: number | undefined;
+  backTime = 0;
+
+  get deckIds() {
+    return this.decks.map((deck) => deck.id);
+  }
+
+  @Mutation
+  addToSchedule(
+    fact_id: number,
+    typed: string,
+    response: boolean,
+    elapsed_seconds_text: number,
+    elapsed_seconds_answer: number,
+  ) {
+    this.schedule.push({
+      fact_id: fact_id,
+      typed: typed,
+      response: response,
+      elapsed_seconds_text: elapsed_seconds_text,
+      elapsed_seconds_answer: elapsed_seconds_answer,
+    });
+  }
+
+  @Mutation
+  emptySchedule() {
+    this.schedule = [];
+  }
+
+  @Mutation
+  setRecommendation(payload: boolean) {
+    this.recommendation = payload;
+  }
+
+  @Mutation
+  setShow(payload: IComponents["Fact"]) {
+    this.show = {
+      text: payload.text,
+      fact: payload,
+      enable_report: true,
+      enable_actions: true,
+    };
+    this.startTimer();
+  }
+
+  @Mutation
+  setShowLoading() {
+    this.show = { text: "Loading...", enable_report: false, enable_actions: false };
+  }
+
+  @Mutation
+  setShowEmpty() {
+    this.show = {
+      text: "You have finished studying these decks for now, check back in later!",
+      enable_report: false,
+      enable_actions: false,
+    };
+  }
+
+  @Mutation
+  setShowError() {
+    this.show = {
+      text: "A problem occurred, check back in later!",
+      enable_report: false,
+      enable_actions: false,
+    };
+  }
 
   @Mutation
   setFacts(payload: IComponents["Fact"][]) {
     this.facts = payload;
   }
 
+  @Mutation
+  removeFirstFact() {
+    this.facts.shift();
+  }
+
+  @Mutation
+  loading() {
+    this.show = { text: "loading", enable_report: false, enable_actions: false };
+  }
+
+  @Mutation
+  updateTimer() {
+    this.time++;
+  }
+
+  @Mutation
+  resetTimer() {
+    clearInterval(this.timer);
+    this.time = 0;
+  }
+
+  @Mutation
+  startTimer() {
+    this.timer = setInterval(() => this.updateTimer(), 1000);
+  }
+
+  @Mutation
+  markFrontTime() {
+    this.frontTime = this.time;
+    this.resetTimer();
+  }
+
+  @Mutation
+  markBackTime() {
+    this.backTime = this.time;
+    this.resetTimer();
+  }
+
   @Action
-  async getFacts(deckIds: number[]) {
+  async getNextShow() {
+    this.resetTimer();
+    if (this.facts.length > 0) {
+      this.setShow(this.facts[0]);
+      this.removeFirstFact();
+    } else {
+      await this.getFacts();
+    }
+  }
+
+  @Action
+  async getFacts() {
+    this.resetTimer();
     try {
-      const response = await api.getStudyFacts(mainStore.token, deckIds);
-      if (response) {
+      this.setShowLoading();
+      const response = await api.getStudyFacts(mainStore.token, this.deckIds);
+      if (response.data.length == 0) {
+        this.setShowEmpty();
+        this.setFacts([]);
+      } else {
         this.setFacts(response.data);
+        await this.getNextShow();
       }
     } catch (error) {
       await mainStore.checkApiError(error);
+      console.log(error)
+      this.setShowError();
+    }
+  }
+
+  @Action
+  async suspendFact() {
+    this.resetTimer();
+    if (this.show.fact && this.show.enable_actions) {
+      try {
+        await api.suspendFact(mainStore.token, this.show.fact.fact_id);
+        await this.getNextShow();
+      } catch (error) {
+        await mainStore.checkApiError(error);
+      }
+    }
+  }
+
+  @Action
+  async reportFact() {
+    this.resetTimer();
+    if (this.show.fact && this.show.enable_report) {
+      try {
+        await api.reportFact(mainStore.token, this.show.fact.fact_id);
+        await this.getNextShow();
+      } catch (error) {
+        await mainStore.checkApiError(error);
+      }
+    }
+  }
+
+  @Action
+  async deleteFact() {
+    this.resetTimer();
+    if (this.show.fact && this.show.enable_actions) {
+      try {
+        await api.deleteFact(mainStore.token, this.show.fact.fact_id);
+        await this.getNextShow();
+      } catch (error) {
+        await mainStore.checkApiError(error);
+      }
+    }
+  }
+
+  @Action
+  async evaluateAnswer(typed: string) {
+    console.log("Evaluate ANSWER");
+    if (this.show.fact && this.show.enable_actions) {
+      try {
+        this.markFrontTime();
+        const response = await api.evalAnswer(
+          mainStore.token,
+          this.show.fact.fact_id,
+          typed,
+        );
+        this.setRecommendation(response.data);
+        this.startTimer();
+      } catch (error) {
+        await mainStore.checkApiError(error);
+      }
+    }
+  }
+
+  @Action
+  async updateSchedule() {
+    if (this.show.fact && this.show.enable_actions) {
+      try {
+        await api.updateSchedule(mainStore.token, this.schedule);
+        this.emptySchedule();
+        await this.getNextShow();
+      } catch (error) {
+        await mainStore.checkApiError(error);
+      }
     }
   }
 }
--- frontend/src/views/main/study/Learn.vue
+++ frontend/src/views/main/study/Learn.vue
@@ -1,79 +1,75 @@
 <template>
   <v-container fluid>
     <v-card class="mx-3 my-1 py-1 px-3">
-      <v-card-title primary-title class="mx-3 my-0 pa-0">
-        <v-col class="headline primary--text ma-0 pa-0">Learn</v-col>
-        <v-btn v-if="$vuetify.breakpoint.xsOnly" text icon @click="dialog = true">
+      <v-card-title v-show="show.enable_actions" primary-title class="mx-3 my-0 pa-0">
+        <div class="headline primary--text ma-0 pa-0">Learn</div>
+        <v-spacer></v-spacer>
+        <v-btn v-if="$vuetify.breakpoint.smAndDown" text icon @click="dialog = true">
           <v-icon>mdi-information</v-icon>
         </v-btn>
         <v-btn v-else class="ma-1 pa-2" @click="dialog = true">
           <v-icon>mdi-information</v-icon>Debug (Alt-/)
         </v-btn>
-        <v-btn v-if="$vuetify.breakpoint.xsOnly" text icon @click="suspend()">
+        <v-btn v-if="$vuetify.breakpoint.smAndDown" text icon @click="suspend()">
           <v-icon>mdi-pause</v-icon>
         </v-btn>
         <v-btn v-else class="ma-1 pa-2" @click="suspend()">
           <v-icon left>mdi-pause</v-icon>Suspend (Alt-S)
         </v-btn>
-        <div v-if="fact && fact.public">
-          <v-btn v-if="$vuetify.breakpoint.xsOnly" text icon @click="deleteOrReport()">
+        <div v-if="show.enable_report">
+          <v-btn v-if="$vuetify.breakpoint.smAndDown" text icon @click="report()">
             <v-icon>mdi-alert-octagon</v-icon>
           </v-btn>
-          <v-btn v-else class="ma-1 pa-2" @click="deleteOrReport()">
+          <v-btn v-else class="ma-1 pa-2" @click="report()">
             <v-icon left>mdi-alert-octagon</v-icon>Report (Alt-R)
           </v-btn>
         </div>
-        <div>
-          <v-btn v-if="$vuetify.breakpoint.xsOnly" text icon @click="deleteOrReport()">
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-          <v-btn v-else class="ma-1 pa-2" @click="deleteOrReport()">
-            <v-icon left>mdi-delete</v-icon>Delete (Alt-R)
-          </v-btn>
-        </div>
+        <v-btn v-if="$vuetify.breakpoint.smAndDown" text icon @click="remove()">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+        <v-btn v-else class="ma-1 pa-2" @click="remove()">
+          <v-icon left>mdi-delete</v-icon>Delete (Alt-R)
+        </v-btn>
       </v-card-title>
     </v-card>
     <v-card class="my-2 mx-3 px-3 py-3">
       <v-card-title class="py-2 pb-0">
         <div
           v-if="
-            !loading &&
-              fact &&
-              fact.category &&
-              fact.category.toString() !== 'None' &&
-              fact.category.toString() !== 'Other'
+            show.enable_actions &&
+              show.fact &&
+              show.fact.category &&
+              show.fact.category.toString() !== 'None' &&
+              show.fact.category.toString() !== 'Other'
           "
           class="title"
         >
-          Category: {{ fact.category }}
+          Category: {{ show.fact.category }}
         </div>
         <div v-else class="title">Front</div>
-        <div v-if="!loading && fact && fact.identifier" class="title">
-          &nbsp;— Identify {{ fact.identifier }}
+        <div
+          v-if="show.enable_actions && show.fact && show.fact.identifier"
+          class="title"
+        >
+          &nbsp; — Identify {{ show.fact.identifier }}
         </div>
       </v-card-title>
       <v-card-text class="pb-2">
-        <div v-if="!loading && fact" class="title primary--text text--darken-2">
-          {{ fact.front }}
-        </div>
-        <div v-else-if="loading" class="title primary--text text--darken-2">
-          Loading next fact...
-        </div>
-        <div v-else class="title primary--text text--darken-2">
-          You have finished your facts for now, check back in later!
+        <div class="title primary--text text--darken-2">
+          {{ show.text }}
         </div>
       </v-card-text>
-      <v-card-text v-show="showAnsButtonAndField" class="pt-3">
+      <v-card-text v-show="show.enable_actions" class="pt-3">
         <v-text-field
           id="answer"
-          v-model="answer"
+          v-model="typed"
           solo
           label="Type Answer"
           autofocus
           class="hide-details"
         ></v-text-field>
       </v-card-text>
-      <v-card-actions v-show="showAnsButtonAndField">
+      <v-card-actions v-show="show.enable_actions">
         <v-btn @click="showAnswer">Show Answer</v-btn>
       </v-card-actions>
     </v-card>
@@ -82,14 +78,12 @@
         <div class="title">Back</div>
       </v-card-title>
       <v-card-text class="pt-2 pb-0">
-        <div v-if="fact" class="title text--darken-2 pb-2">
-          You typed: '{{ answer }}'
-        </div>
-        <div v-if="fact" class="title primary--text text--darken-2 py-2">
-          Answer: {{ fact.back }}
+        <div class="title text--darken-2 pb-2">You typed: '{{ typed }}'</div>
+        <div class="title primary--text text--darken-2 py-2">
+          Answer: {{ show.fact && show.fact.answer }}
         </div>
         <div
-          v-if="evalAnswer"
+          v-if="recommendation"
           class="title primary--text text--darken-2 py-2"
           :style="{ color: 'green !important' }"
         >
@@ -116,16 +110,16 @@
           <v-col class="ma-1 pa-1 shrink">
             <v-btn
               ref="again"
-              :color="!evalAnswer ? 'red' : ''"
-              @click="response('wrong')"
+              :color="!recommendation ? 'red' : ''"
+              @click="response(false)"
               >wrong (1)</v-btn
             >
           </v-col>
           <v-col class="ma-1 pa-1 shrink">
             <v-btn
               ref="good"
-              :color="evalAnswer ? 'green' : ''"
-              @click="response('right')"
+              :color="recommendation ? 'green' : ''"
+              @click="response(true)"
               >right (2)</v-btn
             >
           </v-col>
@@ -139,7 +133,7 @@
         </v-card-title>
         <v-card-text>
           <!-- eslint-disable-next-line vue/no-v-html -->
-          <div v-html="fact && fact.rationale"></div>
+          <div v-html="show.fact && show.fact.rationale"></div>
         </v-card-text>
         <v-card-actions>
           <v-btn @click="dialog = false">Close</v-btn>
@@ -152,74 +146,44 @@
 <script lang="ts">
   import { Component, Vue } from "vue-property-decorator";
   import { studyStore } from "@/utils/store-accessor";
-  // import { IComponents } from "@/interfaces";
 
   @Component
   export default class Learn extends Vue {
-    showAnsButtonAndField = true;
     showBack = false;
-    answer = "";
+    typed = "";
     retyped = "";
-    loading = true;
-    time = 0;
-    frontTime = 0;
-    backTime = 0;
-    timer: number | undefined;
     dialog = false;
-
-    get fact() {
-      if (this.facts.length > 0) {
-        return this.facts[0];
-      } else {
-        return null;
-      }
-      //   if (this.facts.length > 0) {
-      //     return this.facts[0];
-      //   } else {
-      //   }
-    }
 
     get facts() {
       return studyStore.facts;
     }
 
-    get evalAnswer() {
-      return false;
-      //   return readEvalAnswer(this.$store);
+    get show() {
+      return studyStore.show;
+    }
+
+    get frontTime() {
+      return studyStore.frontTime;
+    }
+
+    get backTime() {
+      return studyStore.backTime;
+    }
+
+    get recommendation() {
+      return studyStore.recommendation;
     }
 
     public async mounted() {
-      // const self = this;
-      // this.showBack = false;
-      // this.loading = true;
-      // this.showAnsButtonAndField = false;
-      // this.dialog = false;
-      // this.resetTimer();
-      // this.getFactSet().finally(function() {
-      //   self.showAnsButtonAndField = self.fact !== undefined;
-      //   self.loading = false;
-      //   self.startTimer();
-      // });
-      // this.userProfile;
-      // window.addEventListener("keydown", this.handleKeyPress);
+      await studyStore.getNextShow();
+      window.addEventListener("keydown", this.handleKeyPress);
     }
 
     public async destroyed() {
       // commitResetFacts(this.$store);
+      studyStore.setShowLoading();
+      studyStore.emptySchedule();
       window.removeEventListener("keydown", this.handleKeyPress);
-    }
-
-    public updateTimer() {
-      this.time++;
-    }
-
-    public resetTimer() {
-      clearInterval(this.timer);
-      this.time = 0;
-    }
-
-    public startTimer() {
-      this.timer = setInterval(() => this.updateTimer(), 1000);
     }
 
     public handleKeyPress(e) {
@@ -230,140 +194,75 @@
       } else if (e.altKey && KeyBoardCode == "Slash") {
         this.dialog = !this.dialog;
       } else if (e.altKey && KeyBoardCode == "KeyR") {
-        this.deleteOrReport();
+        this.report();
       } else if (this.showBack) {
         this.determineResponse(KeyBoardCode);
       } else if (KeyBoardCode == "Enter") {
         this.showAnswer();
       } else {
-        //this.$ref.answer.$el.focus() doesn't work some reason
+        //this.$ref.typed.$el.focus() doesn't work some reason
         document.getElementById("answer")!.focus(); // eslint-disable-line
       }
     }
 
-    // public async getFactSet() {
-    //   const queryDeckIdsStrings = this.$router.currentRoute.query.deck;
-    //   if (this.userProfile && this.userProfile.id !== null && queryDeckIdsStrings) {
-    //     let queryDeckIds;
-    //     if (
-    //       typeof queryDeckIdsStrings === "string" ||
-    //       typeof queryDeckIdsStrings === "number"
-    //     ) {
-    //       queryDeckIds = [Number(queryDeckIdsStrings)];
-    //     } else {
-    //       queryDeckIds = queryDeckIdsStrings.map(Number);
-    //     }
-    //     await dispatchGetDeckFacts(this.$store, {
-    //       userId: this.userProfile.id,
-    //       deckIds: queryDeckIds,
-    //     });
-    //   } else if (this.userProfile && this.userProfile.id !== null) {
-    //     await dispatchGetFacts(this.$store, { userId: this.userProfile.id });
-    //   }
-    // }
-
     public determineResponse(KeyBoardCode) {
       switch (KeyBoardCode) {
         case "Enter":
-          // this.response(this.evalAnswer);
+          this.response(this.recommendation);
           break;
         case "Digit1":
-          this.response("wrong");
+          this.response(false);
           break;
         case "Digit2":
-          this.response("right");
+          this.response(true);
           break;
         default:
-          document.getElementById("retype_answer")!.focus();
+          // TODO: Don't use getElementById
+          document.getElementById("retype_answer")!.focus(); //eslint-disable-line
           break;
       }
     }
 
     public async showAnswer() {
-      // if (
-      //   this.showAnsButtonAndField &&
-      //   this.userProfile &&
-      //   this.userProfile.id !== null
-      // ) {
-      //   this.showAnsButtonAndField = false;
-      //   await dispatchEvaluateAnswer(this.$store, {
-      //     factId: this.fact.id,
-      //     typed: this.answer,
-      //   });
-      //   this.showBack = true;
-      //   this.frontTime = this.time;
-      // }
+      console.log("SHOWING ANSWER");
+      await studyStore.evaluateAnswer(this.typed);
+      this.showBack = true;
     }
 
     public resetCard() {
-      // this.showBack = false;
-      // this.answer = "";
-      // this.retyped = "";
-      // this.resetTimer();
-      //
-      // commitRemoveFirstFact(this.$store);
-      // this.showAnsButtonAndField = this.fact !== undefined;
-    }
-
-    public checkEmpty() {
-      // if (this.facts.length === 0) {
-      //   this.loading = true;
-      //   const self = this;
-      //   this.getFactSet().finally(function() {
-      //     self.showAnsButtonAndField = self.fact !== undefined;
-      //     self.loading = false;
-      //
-      //     self.resetTimer();
-      //     self.startTimer();
-      //   });
-      // } else {
-      //   this.resetTimer();
-      //   this.startTimer();
-      // }
+      this.showBack = false;
+      this.typed = "";
+      this.retyped = "";
     }
 
     public async suspend() {
-      // await dispatchSuspendFact(this.$store, this.fact.id);
-
+      await studyStore.suspendFact();
       this.resetCard();
-      this.checkEmpty();
     }
 
-    public async deleteOrReport() {
-      // await dispatchDeleteFact(this.$store, this.fact.id);
-
+    public async report() {
+      await studyStore.reportFact();
       this.resetCard();
-      this.checkEmpty();
     }
 
-    public async response(_responseButtonText) {
-      //   const oldFact = this.fact;
-      //   const typed = this.answer;
-      //   this.backTime = this.time - this.frontTime;
-      //
-      //   if (await this.$validator.validateAll()) {
-      //     if (this.userProfile && this.userProfile.id !== null) {
-      //       const factUpdate: FactScheduleUpdate = {
-      //         review_datetime: new Date().toISOString(),
-      //         front: oldFact.front,
-      //         back: oldFact.back,
-      //         typed: typed,
-      //         response: responseButtonText,
-      //         deck_id: oldFact.deck_id,
-      //         elapsed_seconds_front: this.frontTime < 30 ? this.frontTime : 30,
-      //         elapsed_seconds_back: this.backTime < 30 ? this.backTime : 30,
-      //         answer_lines: oldFact.answer_lines,
-      //       };
-      //       const fact_ids: number[] = [oldFact.id];
-      //       await dispatchUpdateFactSchedule(this.$store, {
-      //         factIds: fact_ids,
-      //         update: [factUpdate],
-      //       });
-      //     }
-      //   }
-      //
-      //   this.resetCard();
-      //   this.checkEmpty();
+    public async remove() {
+      await studyStore.deleteFact();
+      this.resetCard();
+    }
+
+    public async response(response) {
+      if (this.show.fact) {
+        studyStore.markBackTime();
+        studyStore.addToSchedule(
+          this.show.fact.fact_id,
+          this.typed,
+          response,
+          this.frontTime,
+          this.backTime,
+        );
+        await studyStore.updateSchedule();
+        this.resetCard();
+      }
     }
   }
 </script>
