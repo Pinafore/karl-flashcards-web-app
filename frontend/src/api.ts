9126c7eef5cd6326bda845c8e0ab4295e024fbdb
Nonameentered <matthewshu88@gmail.com>
2020-05-05 02:51:52 -0400

    Add api axios calls

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -63,4 +63,56 @@ export const api = {
     }
     return axios.get<IComponents["Fact"][]>(url, authHeaders(token));
   },
+  async createFact(token: string, data: IComponents["FactCreate"]) {
+    return axios.post(`${apiUrl}/api/facts/`, data, authHeaders(token));
+  },
+  async createDeck(token: string, data: IComponents["DeckCreate"]) {
+    return axios.post(`${apiUrl}/api/decks/`, data, authHeaders(token));
+  },
+  async suspendFact(token: string, factId: number, undo?: boolean) {
+    let url = `${apiUrl}/api/facts/suspend/${factId}`;
+    if (undo) {
+      url += undo;
+    }
+    return axios.put<IComponents["Fact"]>(url, null, authHeaders(token));
+  },
+  async reportFact(token: string, factId: number, undo?: boolean) {
+    let url = `${apiUrl}/api/facts/report/${factId}`;
+    if (undo) {
+      url += undo;
+    }
+    return axios.put<IComponents["Fact"]>(url, null, authHeaders(token));
+  },
+  async deleteFact(token: string, factId: number, undo?: boolean) {
+    let url = `${apiUrl}/api/facts/${factId}`;
+    if (undo) {
+      url += undo;
+    }
+    return axios.delete<IComponents["Fact"]>(url, authHeaders(token));
+  },
+  async getPublicDecks(token: string) {
+    return axios.get<IComponents["Deck"][]>(
+      `${apiUrl}/api/decks/public/`,
+      authHeaders(token),
+    );
+  },
+  async assignDecks(token: string, deckIds: number[]) {
+    let url = `${apiUrl}/api/decks/`;
+    if (deckIds.length > 0) {
+      url += `?`;
+      for (const eachId in deckIds) {
+        url += `&deck_ids=${deckIds[eachId]}`;
+      }
+    }
+    return axios.put(url, null, authHeaders(token));
+  },
+  async evalAnswer(token: string, factId: number, data: string) {
+    return axios.get<boolean>(
+      `${apiUrl}/api/study/evaluate?fact_id=${factId}&typed=${data}`,
+      authHeaders(token),
+    );
+  },
+  async updateSchedule(token: string, data: IComponents["Schedule"][]) {
+    return axios.put(`${apiUrl}/api/study/`, data, authHeaders(token));
+  },
 };
