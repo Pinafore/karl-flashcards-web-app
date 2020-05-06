5bfbc52873e4fd9118ab4aea8322a84594f6c5f3
Nonameentered <matthewshu88@gmail.com>
2020-05-06 12:53:43 -0400

    redirect to public decks on sign up

--- frontend/src/store/modules/main.ts
+++ frontend/src/store/modules/main.ts
@@ -199,12 +199,10 @@ export default class MainModule extends VuexModule {
 
   @Action
   async routeLoggedIn() {
-    if (
-      router.currentRoute.path === "/sign-up" ||
-      router.currentRoute.path === "/login" ||
-      router.currentRoute.path === "/"
-    ) {
-      router.push("/main");
+    if (router.currentRoute.path === '/login' || router.currentRoute.path === '/') {
+      router.push('/main');
+    } else if (router.currentRoute.path === '/sign-up') {
+      router.push('main/add/public-decks')
     }
   }
 
