a85ecd12540466f0e25e104674512de8f0326469
Nonameentered <matthewshu88@gmail.com>
2020-06-02 20:48:43 -0400

    rename some stuff from email to username

--- backend/app/app/api/api_v1/endpoints/login.py
+++ backend/app/app/api/api_v1/endpoints/login.py
@@ -66,7 +66,7 @@ def recover_password(email: str, db: Session = Depends(deps.get_db)) -> Any:
         )
     password_reset_token = generate_password_reset_token(email=email)
     send_reset_password_email(
-        email_to=user.email, email=email, token=password_reset_token
+        email_to=user.email, username=user.username, token=password_reset_token
     )
     return {"msg": "Password recovery email sent"}
 
--- backend/app/app/utils/utils.py
+++ backend/app/app/utils/utils.py
@@ -46,9 +46,9 @@ def send_test_email(email_to: str) -> None:
     )
 
 
-def send_reset_password_email(email_to: str, email: str, token: str) -> None:
+def send_reset_password_email(email_to: str, username: str, token: str) -> None:
     project_name = settings.PROJECT_NAME
-    subject = f"{project_name} - Password recovery for user {email}"
+    subject = f"{project_name} - Password recovery for user {username}"
     with open(Path(settings.EMAIL_TEMPLATES_DIR) / "reset_password.html") as f:
         template_str = f.read()
     server_host = settings.SERVER_HOST
@@ -59,7 +59,7 @@ def send_reset_password_email(email_to: str, email: str, token: str) -> None:
         html_template=template_str,
         environment={
             "project_name": settings.PROJECT_NAME,
-            "username": email,
+            "username": username,
             "email": email_to,
             "valid_hours": settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS,
             "link": link,
