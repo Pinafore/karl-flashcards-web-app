02360882443dff18206dfe3dc9f3aae6312a399c
Nonameentered <matthewshu88@gmail.com>
2020-06-25 12:31:46 -0400

    use vuegtag instead of vueanalytics

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,22 +7,24 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import * as Sentry from "@sentry/browser";
 import { Vue as VueIntegration } from "@sentry/integrations";
-import VueAnalytics from 'vue-analytics';
+import VueGtag from "vue-gtag"
 
 if (process.env.VUE_APP_ENV) {
   Sentry.init({
     dsn: "https://ac296d2d7e8c4115ab8f2713520612cf@o283930.ingest.sentry.io/5259730",
     integrations: [new VueIntegration({ Vue, attachProps: true, logErrors: true })],
   });
+
 }
 
 
 Vue.config.productionTip = false;
 
-Vue.use(VueAnalytics, {
-  id: 'G-9X7E66LXQM',
-  router
-});
+Vue.use(VueGtag, {
+  config: { id: "G-9X7E66LXQM" },
+  appName: 'KAR³L',
+  pageTrackerScreenviewEnabled: true
+}, router);
 
 new Vue({
   router,
