7cd185329bb8dc1af2256f17272f37ec9f7f3208
Nonameentered <matthewshu88@gmail.com>
2020-08-06 00:10:44 -0400

    add karl50 and karl85 to spaced repetition systems

--- backend/app/app/schemas/repetition.py
+++ backend/app/app/schemas/repetition.py
@@ -6,6 +6,8 @@ class Repetition(str, Enum):
     leitner = "leitner"
     sm2 = "sm-2"
     karl = "karl"
+    karl50 = "karl50"
+    karl85 = "karl85"
 
     @classmethod
     def select_model(cls):
