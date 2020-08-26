8de78e326525a4c9915b7f3b1123bda11a347646
Nonameentered <matthewshu88@gmail.com>
2020-08-25 23:53:53 -0400

    additional info in contact

--- frontend/src/views/main/Contact.vue
+++ frontend/src/views/main/Contact.vue
@@ -8,18 +8,28 @@
           </v-card-title>
           <v-card-text>
             Do you have bugs to report or comments for the developers? Join the official
-            KAR³L Discord or contact us through email!
+            KAR³L Discord, comment on our quizbowl forum post, or contact us through
+            email!
           </v-card-text>
           <v-card-actions class="pt-0">
-            <v-spacer></v-spacer>
-            <v-btn color="primary" text href="https://discord.gg/PTfEmHd">
-              Join Discord
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" text href="mailto:learningwithkarl@gmail.com">
-              Email Us
-            </v-btn>
-            <v-spacer></v-spacer>
+            <v-row>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text href="https://discord.gg/PTfEmHd">
+                Join Discord </v-btn
+              ><v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                href="https://hsquizbowl.org/forums/viewtopic.php?f=123&p=379140&sid=1edf62c02b590220bc42d0104791bca6#p379140"
+              >
+                Forum Post
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text href="mailto:learningwithkarl@gmail.com">
+                Email Us
+              </v-btn>
+              <v-spacer></v-spacer>
+            </v-row>
           </v-card-actions>
         </v-card>
       </v-col>
