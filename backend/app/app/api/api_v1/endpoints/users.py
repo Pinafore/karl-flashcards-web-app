6f95b4c6012a3366b9353601c4e64feb8b2c66e0
Nonameentered <matthewshu88@gmail.com>
2021-09-10 01:01:25 -0400

    disable public sign ups

--- backend/app/app/api/api_v1/endpoints/users.py
+++ backend/app/app/api/api_v1/endpoints/users.py
@@ -114,6 +114,11 @@ def create_user_open(
     """
     Create new user without the need to be logged in.
     """
+    raise HTTPException(
+        status_code=512,
+        detail="New user sign up is currently disabled",
+    )
+    
     if not settings.USERS_OPEN_REGISTRATION:
         raise HTTPException(
             status_code=403,
