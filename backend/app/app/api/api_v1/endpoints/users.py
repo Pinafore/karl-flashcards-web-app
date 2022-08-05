ae909db6fcbc36416cf852f8e00bf190558f475c
Matt Shu <matthewshu88@gmail.com>
2022-08-05 02:33:38 -0400

    don't send account creation email to save email quota

--- backend/app/app/api/api_v1/endpoints/users.py
+++ backend/app/app/api/api_v1/endpoints/users.py
@@ -130,10 +130,10 @@ def create_user_open(
     user_in = schemas.UserCreate(password=user_in.password, email=user_in.email, username=user_in.username)
     user = crud.user.create(db, obj_in=user_in)
 
-    if settings.EMAILS_ENABLED and user_in.email:
-        send_new_account_email(
-            email_to=user_in.email, username=user_in.username
-        )
+    # if settings.EMAILS_ENABLED and user_in.email:
+    #     send_new_account_email(
+    #         email_to=user_in.email, username=user_in.username
+    #     )
     return user
 
 
