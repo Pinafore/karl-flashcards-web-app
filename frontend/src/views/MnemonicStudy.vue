8117765d79c89482e8ad271c3aff5da9220d20ca
Nishant Balepur <nishantbalepur@gmail.com>
2024-03-28 15:04:52 -0400

    update mnemonic popups

--- frontend/src/views/MnemonicStudy.vue
+++ frontend/src/views/MnemonicStudy.vue
@@ -1,18 +1,43 @@
 <template class="pa-0 ma-0">
-    <v-main class="pa-0 ma-0">
-      <v-dialog v-model="mnemonic_study_tip" width="1000" @click:outside="exit()">
-        <v-card>
+  <v-main class="pa-0 ma-0">
+    <v-dialog v-model="mnemonic_study_tip" @click:outside="exit()">
+      <v-card>
         <v-card-title>
-          <h2>Mnemonic Research Study</h2>
+          <v-col cols="12" md="7" lg="6" class="pl-0 ml-0">
+            <h2>University of Maryland Research Study</h2>
+          </v-col>
+          <v-col cols="12" md="5" lg="6">
+            <v-img
+              :src="require('@/assets/umd-horizontal.png')"
+              max-height="50"
+              contain
+            ></v-img>
+          </v-col>
         </v-card-title>
         <div class="px-2">
-          <v-card-text>
-            If you study our GRE Vocab deck in KAR³L, you will be participating in our research study,
-            which spans from 02/26/2024 to 05/26/2024. In this research study, we will
-            be collecting your feedback to improve our mnemonic device generation. None
-            of your personal information will be collected or released. Users
-            participating in our study are eligible to receive <b>monetary rewards</b>,
-            summarized below. Full instructions for our user study can be found
+          <v-card-text class="subtitle-1">
+              We are a team of researchers at the University of Maryland who have
+              recently developed KAR³L, the first flashcard scheduler powered by machine
+              learning and natural language processing (NLP). Our hope is that with these
+              advanced techniques, we can research and develop methods to help students learn more
+              effectively. If you are interested in learning more about our mission, you
+              can visit our <a target="_blank" href="https://nbalepur.github.io/assets/html/KARL">research project website</a>
+              .
+              At this point, we are conducting a funded user study to see if
+              model-generated mnemonic devices can help students more effectively
+              (described below). Come join us to help us build the next generation of
+              NLP-powered adaptive learning tools!
+          </v-card-text>
+
+          <v-card-text class="subtitle-1">
+            If you study our deck titled "GRE Vocab" in KAR³L, you will be participating in our
+            research study with the University of Maryland, which spans from 02/26/2024
+            to 05/26/2024. In this research study, we will be collecting your feedback
+            to improve our mnemonic device generation. None of your personal information
+            will be collected or released. Users participating in our study are eligible
+            to receive
+            <b>monetary rewards</b>, summarized below. Full instructions for our user
+            study can be found
             <a
               target="_blank"
               href="https://docs.google.com/document/d/1Ecv2BbHN_AAsmv7Orf7IGCu8ENGbC5mRyRKcNKjc3fU/edit?usp=sharing"
@@ -22,7 +47,7 @@
           <v-card-title>
             Monetary Reward Structure
           </v-card-title>
-          <v-card-text>
+          <v-card-text class="subtitle-1">
             As a token of appreciation for helping us with our study, we will have two
             ways for users to win rewards (both rewards can be earned):
             <ol>
@@ -38,7 +63,8 @@
               </li>
             </ol>
             <br />
-            You can check out the "statistics" page to see your progress on these rewards! (see "Vocab Facts Studied" and "Mnemonics Rated")
+            You can check out the "statistics" page to see your progress on these
+            rewards! (see "Vocab Facts Studied" and "Mnemonics Rated")
           </v-card-text>
           <!-- <v-card-title>
             Notice on Accurate Ratings
@@ -53,9 +79,9 @@
           <v-card-title>
             Contact
           </v-card-title>
-          <v-card-text>
-            If you have any questions, please contact me (Nishant Balepur), a
-            co-investigator of the project (University of Maryland,
+          <v-card-text class="subtitle-1">
+            If you have any questions or concerns, please feel to reach out to me
+            (Nishant Balepur), a co-investigator of the project (University of Maryland,
             <a target="_blank" href="mailto:nbalepur@umd.edu">nbalepur@umd.edu</a>). You
             can also join our
             <a target="_blank" href="https://discord.gg/PTfEmHd">Discord</a>. Thanks
@@ -74,41 +100,30 @@
           </v-btn>
         </v-card-actions>
       </v-card>
-      </v-dialog>
-    </v-main>
-  </template>
-  
-  <script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
-    import { mainStore } from "@/store";
-  
-    @Component
-    export default class MnemonicStudy extends Vue {
-      mnemonic_study_tip = false;
+    </v-dialog>
+  </v-main>
+</template>
 
-      async mounted() {
-        await mainStore.getUserProfile();
-        this.mnemonic_study_tip =
-          (mainStore.userProfile?.show_mnemonic_help ?? false) ||
-          this.$router.currentRoute.name === "mnemonic-study";
-      }
-  
-      noMoreMnemonicStudy() {
-        if (this.$router.currentRoute.name === "mnemonic-study") {
-          this.$router.push("/landing");
-        }
-        mainStore.updateUserHelpMnemonic(false);
-        this.mnemonic_study_tip = false;
-      }
-  
-      exit() {
-        this.mnemonic_study_tip = false;
-        if (this.$router.currentRoute.name === "mnemonic-study") {
-          this.$router.push("/landing");
-        }
+<script lang="ts">
+  import { Component, Vue } from "vue-property-decorator";
+  import { mainStore } from "@/store";
+
+  @Component
+  export default class MnemonicStudy extends Vue {
+    mnemonic_study_tip = false;
+
+    async mounted() {
+      this.mnemonic_study_tip = this.$router.currentRoute.name === "mnemonic-study";
+    }
+
+    exit() {
+      this.mnemonic_study_tip = false;
+      if (this.$router.currentRoute.name === "mnemonic-study") {
+        console.log('pushing');
+        this.$router.push("/landing");
       }
     }
-  </script>
-  
-  <style></style>
-  
+  }
+</script>
+
+<style></style>
